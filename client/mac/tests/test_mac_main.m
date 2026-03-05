7df6dfc23c37d3ae1f4a878a546942551c7564c4
Jeffrey Bergier <jeffburg@jeffburg.com>
2026-03-05 10:54:16 +0000

    [Vibe] Separated makefile for Mac

--- client/mac/tests/test_mac_main.m
+++ client/mac/tests/test_mac_main.m
@@ -1,14 +1,13 @@
-#import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
 /**
- * Main entry point for iOS Application unit tests.
+ * Main entry point for macOS Application unit tests.
  * Library tests are handled by their respective modules.
  */
 int main(int argc, char *argv[]) {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
   
-  NSLog(@"Running iOS Application Unit Tests...");
+  NSLog(@"Running macOS Application Unit Tests...");
   
   // This runner can be used for app-level UI or integration tests later.
   // For now, it just initializes the XCTest environment.
